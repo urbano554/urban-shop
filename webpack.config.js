f6bf5deaf41e6529906ca951041b6660332e90bd
luis urbano <urbanoluismanuel54@gmail.com>
2022-12-13 00:07:29 -0400

    Fix(deploy)

--- webpack.config.js
+++ webpack.config.js
@@ -3,68 +3,65 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js',
-	},
-	resolve: {
-		extensions: ['.js', '.jsx'],
-		alias: {
-			'@components': path.resolve(__dirname, 'src/components/'),
-			'@containers': path.resolve(__dirname, 'src/containers/'),
-			'@pages': path.resolve(__dirname, 'src/pages/'),
-			'@routes': path.resolve(__dirname, 'src/routes/'),
-			'@styles': path.resolve(__dirname, 'src/styles/'),
-			'@icons': path.resolve(__dirname, 'src/assets/icons/'),
-			'@logos': path.resolve(__dirname, 'src/assets/logos/'),
-		}
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader'
-				}
-			},
-			{
-				test: /\.js$/,
-				enforce: 'pre',
-				use: ['source-map-loader'],
-			  },
-			{
-				test: /\.html$/,
-				use: [
-					{
-						loader: 'html-loader'
-					}
-				]
-			},
-			{
-				test: /\.(css|scss)$/,
-				use: [
-					"style-loader",
-					"css-loader",
-					"sass-loader",
-				],
-			},
-			{
-				test: /\.(png|svg|jpg|gif)$/,
-				type: 'asset'
-			}
-		]
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: './public/index.html',
-			filename: './index.html'
-		}),
-		new MiniCssExtractPlugin({
-			filename: '[name].css'
-		}),
-	],
-	devServer: {
-		historyApiFallback: true,
-	}
-}
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: 'auto',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components/'),
+      '@containers': path.resolve(__dirname, 'src/containers/'),
+      '@pages': path.resolve(__dirname, 'src/pages/'),
+      '@routes': path.resolve(__dirname, 'src/routes/'),
+      '@styles': path.resolve(__dirname, 'src/styles/'),
+      '@icons': path.resolve(__dirname, 'src/assets/icons/'),
+      '@logos': path.resolve(__dirname, 'src/assets/logos/'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        use: ['source-map-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        type: 'asset',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+  },
+};
