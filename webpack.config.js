e512bd38a8f183a2eeeae828c0f88416c6a31b07
luis urbano <urbanoluismanuel54@gmail.com>
2022-12-12 23:32:56 -0400

    Fix(deploy)

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-	entry: './src/index.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'bundle.js',
-		publicPath: '/'
 	},
 	resolve: {
 		extensions: ['.js', '.jsx'],
