4dde032cca9d4ca95f582fa5f21231b20f1bfee9
luis urbano <urbanoluismanuel54@gmail.com>
2022-12-19 14:12:10 -0400

    Feat(multiplies-changes)
    Created two news icons (MinusIcion and PlusIcon)
    
    Added count state for each item in the shopping car for add more same items (src/component/OrderItem.js)
    
    Change logic for the shoppingCar icon
    
    Change Checkout text to Comprar in the MyOrder button (src/component/MyOrder.js)
    
    Added react-toastify in the proyect
    
    Added ToastContainer in the ProductList component
    
    Added logic for show toast  when add or remove one item (src/hooks/useInitialState.js)
    
    Added properties in the title-container from styles/MyOrder.scss
    
    Added count styles in the OrderItem.scss
    
    Changed shoppingCar color to  white  when hover is active

--- src/components/OrderItem.js
+++ src/components/OrderItem.js
@@ -1,28 +1,63 @@
-import { useContext } from 'react';
-import '@styles/OrderItem.scss';
-import close from '@icons/icon_close.png';
+import { useContext, useState } from 'react';
+
 import { AppContext } from '../context/AppContext';
+
+import { MinusIcon } from '@icons/MinusIcon';
+import { PlusIcon } from '@icons/PlusIcon';
+import close from '@icons/icon_close.png';
+
+import '@styles/OrderItem.scss';
 
 const OrderItem = ({ title, price, image, id }) => {
   const { removeProductFromCart } = useContext(AppContext);
+  const [count, setCount] = useState(1);
 
   const handleRemove = () => {
     removeProductFromCart(id);
   };
 
+  const handleIncrease = () => {
+    //TODO
+    // let tempCart = [...state.cart];
+    // const selectedProduct = tempCart.find((item) => item.id === id);
+    // const index = tempCart.indexOf(selectedProduct);
+    // const product = tempCart[index];
+
+    setCount(count + 1);
+  };
+
+  const handleDecrease = () => {
+    if (count < 2) {
+      return;
+    } else {
+      setCount(count - 1);
+    }
+  };
+
   return (
-    <div className='OrderItem'>
-      <figure>
-        <img src={image} />
-      </figure>
-      <p>{title}</p>
-      <p>${price}</p>
-      <img
-        src={close}
-        alt='close'
-        onClick={() => handleRemove(id)}
-      />
-    </div>
+    <>
+      <div className='OrderItem'>
+        <figure>
+          <img src={image} />
+        </figure>
+        <p>{title}</p>
+        <p>${price}</p>
+        <img
+          src={close}
+          alt='close'
+          onClick={() => handleRemove(id)}
+        />
+      </div>
+      <div className='counter-container'>
+        <button onClick={handleDecrease}>
+          <MinusIcon />
+        </button>
+        <p>{count}</p>
+        <button onClick={handleIncrease}>
+          <PlusIcon />
+        </button>
+      </div>
+    </>
   );
 };
 
--- src/containers/ProductList.js
+++ src/containers/ProductList.js
@@ -2,6 +2,7 @@ import { useGetProducts } from '../hooks/useGetProducts';
 
 import ProductItem from '@components/ProductItem';
 import { SkeletonCard } from '@components/SkeletonCard';
+import { ToastContainer } from 'react-toastify';
 
 import { PRODUCT_LIST_API } from '../common/constants';
 
@@ -11,19 +12,22 @@ const ProductList = () => {
   const { products, loading } = useGetProducts(PRODUCT_LIST_API);
 
   return (
-    <section className='main-container'>
-      <div className='ProductList'>
-        {loading && <SkeletonCard />}
-        {products?.map((props) => {
-          return (
-            <ProductItem
-              {...props}
-              key={props.id}
-            />
-          );
-        })}
-      </div>
-    </section>
+    <>
+      <ToastContainer />
+      <section className='main-container'>
+        <div className='ProductList'>
+          {loading && <SkeletonCard />}
+          {products?.map((props) => {
+            return (
+              <ProductItem
+                {...props}
+                key={props.id}
+              />
+            );
+          })}
+        </div>
+      </section>
+    </>
   );
 };
 
